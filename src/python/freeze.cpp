bd805def510a450cc5d913291bc75ff9bfcb7717
Christian Döring <christian.doering@tum.de>
2024-05-05 20:58:43 +0200

    Added traversal of input/output variables

--- src/python/freeze.cpp
+++ src/python/freeze.cpp
@@ -1,15 +1,70 @@
 
 #include "freeze.h"
+#include "apply.h"
+#include "drjit-core/jit.h"
 
 static const char *doc_freeze = R"(
     
 )";
 
 nb::object freeze(nb::callable func){
-    
-    auto new_func = nb::cpp_function([=](nb::args args) {
+
+
+    struct FlatVariables: TraverseCallback{
+        std::vector<uint32_t> variables;
+        JitBackend backend;
         
-        return func(*args);
+        bool inc_ref;
+
+        FlatVariables() : inc_ref(false) {
+        }
+
+        void operator()(nb::handle h) override {
+            auto s = supp(h.type());
+            if (s.index)
+                operator()(s.index(inst_ptr(h)));
+
+            JitBackend var_backend = (JitBackend)s.backend;
+            if(this->backend == var_backend || this->backend == JitBackend::None){
+                this->backend = var_backend;
+            }else{
+                jit_fail("freeze(): backend missmatch error (backend %u does "
+                         "not match %u)!",
+                         (uint32_t)this->backend, (uint32_t)var_backend);
+            }
+        }
+
+        void operator()(uint64_t index) override {
+            if (inc_ref)
+                ad_var_inc_ref(index);
+            variables.push_back(index);
+        }
+    };
+    
+    auto new_func = nb::cpp_function([func](nb::args args) {
+
+        FlatVariables in_variables;
+        traverse("freeze", in_variables, args);
+        
+        JitBackend backend = in_variables.backend;
+
+        jit_record_start(backend, in_variables.variables.data(), in_variables.variables.size());
+
+        auto result = func(*args);
+        
+        FlatVariables out_variables;
+        traverse("freeze", out_variables, result);
+        if(out_variables.backend != backend){
+            jit_fail("freeze(): backend missmatch error (backend %u of output "
+                     "variables did not match backend %u of input variables)",
+                     (uint32_t)out_variables.backend, (uint32_t)backend);
+        }
+
+        Recording *record = jit_record_stop(backend, out_variables.variables.data(), out_variables.variables.size());
+
+        jit_record_destroy(record);
+        
+        return result;
     });
 
     return new_func;
