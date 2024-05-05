b51b5e557534be76dca340e15238158f34c5445b
Christian Döring <christian.doering@tum.de>
2024-05-05 20:58:43 +0200

    Added traversal of input/output variables

--- tests/test_freeze.py
+++ tests/test_freeze.py
@@ -3,12 +3,14 @@ import pytest
 
 @pytest.test_arrays('uint32, jit, shape=(*)')
 def test01_basic(t):
+    dr.set_log_level(dr.LogLevel.Info)
 
     @dr.freeze
-    def func(x):
-        return x + 1
+    def func(x, y):
+        return x + y
 
     i0 = t(0, 1, 2)
-    o0 = func(i0)
+    i1 = t(2, 1, 0)
+    o0 = func(i0, i1)
 
-    assert dr.all(t(1, 2, 3) ==  o0)
+    assert dr.all(t(2, 2, 2) ==  o0)
