6e6c4049ddce65b0830ab044d57ef0a6b10a7639
Christian Döring <christian.doering@tum.de>
2024-05-06 15:07:11 +0200

    Added more output constructors

--- tests/test_freeze.py
+++ tests/test_freeze.py
@@ -1,7 +1,9 @@
 import drjit as dr
 import pytest
+from dataclasses import dataclass
 
-@pytest.test_arrays('uint32, jit, shape=(*)')
+
+@pytest.test_arrays("uint32, jit, shape=(*)")
 def test01_basic(t):
     dr.set_log_level(dr.LogLevel.Info)
 
@@ -11,12 +13,172 @@ def test01_basic(t):
 
     i0 = t(0, 1, 2)
     i1 = t(2, 1, 0)
-    
+
     o0 = func(i0, i1)
-    assert dr.all(t(2, 2, 2) ==  o0)
-    
+    assert dr.all(t(2, 2, 2) == o0)
+
     i0 = t(1, 2, 3)
     i1 = t(3, 2, 1)
-    
+
     o0 = func(i0, i1)
-    assert dr.all(t(4, 4, 4) ==  o0)
+    assert dr.all(t(4, 4, 4) == o0)
+
+
+@pytest.test_arrays("uint32, jit, shape=(*)")
+def test02_output_tuple(t):
+    dr.set_log_level(dr.LogLevel.Info)
+
+    @dr.freeze
+    def func(x, y):
+        return (x + y, x * y)
+
+    i0 = t(0, 1, 2)
+    i1 = t(2, 1, 0)
+
+    (o0, o1) = func(i0, i1)
+    assert dr.all(t(2, 2, 2) == o0)
+    assert dr.all(t(0, 1, 0) == o1)
+
+    i0 = t(1, 2, 3)
+    i1 = t(3, 2, 1)
+
+    (o0, o1) = func(i0, i1)
+    assert dr.all(t(4, 4, 4) == o0)
+    assert dr.all(t(3, 4, 3) == o1)
+
+
+@pytest.test_arrays("uint32, jit, shape=(*)")
+def test03_output_list(t):
+    dr.set_log_level(dr.LogLevel.Info)
+
+    @dr.freeze
+    def func(x, y):
+        return [x + y, x * y]
+
+    i0 = t(0, 1, 2)
+    i1 = t(2, 1, 0)
+
+    [o0, o1] = func(i0, i1)
+    assert dr.all(t(2, 2, 2) == o0)
+    assert dr.all(t(0, 1, 0) == o1)
+
+    i0 = t(1, 2, 3)
+    i1 = t(3, 2, 1)
+
+    [o0, o1] = func(i0, i1)
+    assert dr.all(t(4, 4, 4) == o0)
+    assert dr.all(t(3, 4, 3) == o1)
+
+
+@pytest.test_arrays("uint32, jit, shape=(*)")
+def test04_output_dict(t):
+    dr.set_log_level(dr.LogLevel.Info)
+
+    @dr.freeze
+    def func(x, y):
+        return {"add": x + y, "mul": x * y}
+
+    i0 = t(0, 1, 2)
+    i1 = t(2, 1, 0)
+
+    o = func(i0, i1)
+    o0 = o["add"]
+    o1 = o["mul"]
+    assert dr.all(t(2, 2, 2) == o0)
+    assert dr.all(t(0, 1, 0) == o1)
+
+    i0 = t(1, 2, 3)
+    i1 = t(3, 2, 1)
+
+    o = func(i0, i1)
+    o0 = o["add"]
+    o1 = o["mul"]
+    assert dr.all(t(4, 4, 4) == o0)
+    assert dr.all(t(3, 4, 3) == o1)
+
+
+@pytest.test_arrays("uint32, jit, shape=(*)")
+def test05_nested_tuple(t):
+    dr.set_log_level(dr.LogLevel.Info)
+
+    @dr.freeze
+    def func(x):
+        return (x + 1, x + 2, (x + 3, x + 4))
+
+    i0 = t(0, 1, 2)
+
+    (o0, o1, (o2, o3)) = func(i0)
+    assert dr.all(t(1, 2, 3) == o0)
+    assert dr.all(t(2, 3, 4) == o1)
+    assert dr.all(t(3, 4, 5) == o2)
+    assert dr.all(t(4, 5, 6) == o3)
+
+    i0 = t(1, 2, 3)
+
+
+@pytest.test_arrays("uint32, jit, shape=(*)")
+def test06_drjit_struct(t):
+    dr.set_log_level(dr.LogLevel.Info)
+
+    class Point:
+        x: t
+        y: t
+        DRJIT_STRUCT = {"x": t, "y": t}
+
+    @dr.freeze
+    def func(x):
+        p = Point()
+        p.x = x+1
+        p.y = x+2
+        return p
+
+    i0 = t(0, 1, 2)
+
+    o = func(i0)
+    o0 = o.x
+    o1 = o.y
+    assert dr.all(t(1, 2, 3) == o0)
+    assert dr.all(t(2, 3, 4) == o1)
+    
+    i0 = t(1, 2, 3)
+
+    o = func(i0)
+    o0 = o.x
+    o1 = o.y
+    assert dr.all(t(2, 3, 4) == o0)
+    assert dr.all(t(3, 4, 5) == o1)
+    
+    
+@pytest.test_arrays("uint32, jit, shape=(*)")
+def test06_dataclass(t):
+    dr.set_log_level(dr.LogLevel.Info)
+
+    @dataclass
+    class Point:
+        x: t
+        y: t
+        def __init__(self):
+            ...
+
+    @dr.freeze
+    def func(x):
+        p = Point()
+        p.x = x+1
+        p.y = x+2
+        return p
+
+    i0 = t(0, 1, 2)
+
+    o = func(i0)
+    o0 = o.x
+    o1 = o.y
+    assert dr.all(t(1, 2, 3) == o0)
+    assert dr.all(t(2, 3, 4) == o1)
+    
+    i0 = t(1, 2, 3)
+
+    o = func(i0)
+    o0 = o.x
+    o1 = o.y
+    assert dr.all(t(2, 3, 4) == o0)
+    assert dr.all(t(3, 4, 5) == o1)
